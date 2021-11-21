925c3a2f253c0354bfbf78d34c80bbb78388e1af
larandias1d <84997346+larandias1d@users.noreply.github.com>
2021-11-21 13:33:56 -0300

    Delete src directory

--- src/main/java/br/edu/iff/projetoSupermercado/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.iff.projetoSupermercado.exception;
-
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
